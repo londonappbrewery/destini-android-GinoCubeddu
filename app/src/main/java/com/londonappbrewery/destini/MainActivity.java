deca4edba4c4a5c98d27b9e1cf4f894069c9b639
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-02-14 13:15:56 +0000

    Create a dict to hold the levels
    A dict has been created with the key being the id
    for the story level and the value being the
    level instace for that story

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
@@ -14,22 +21,49 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonTop;
     private Button mButtonBottom;
 
+    // Will hold the possible paths
+    private HashMap<Integer, Level> mPath;
+
+    // Holds the current story id
+    private int mCurrentStory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
         mStory = (TextView) findViewById(R.id.storyTextView);
         mButtonTop = (Button) findViewById(R.id.buttonTop);
         mButtonBottom = (Button) findViewById(R.id.buttonBottom);
 
+        mCurrentStory = R.string.T1_Story;
+        mPath = new HashMap<Integer, Level>();
+
+        // Level T1
+        mPath.put(
+            R.string.T1_Story,
+            new Level(R.string.T1_Ans1, R.string.T1_Ans2, R.string.T3_Story, R.string.T2_Story)
+        );
+
+        // Level T2
+        mPath.put(
+            R.string.T2_Story,
+            new Level(R.string.T2_Ans1, R.string.T2_Ans2, R.string.T3_Story, R.string.T4_End)
+        );
+
+        // Level T3
+        mPath.put(
+            R.string.T3_Story,
+            new Level(R.string.T3_Ans1, R.string.T3_Ans2, R.string.T6_End, R.string.T5_End)
+        );
+
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
         mButtonTop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("destini", "top button pressed");
+                mCurrentStory = mPath.get(mCurrentStory).getPath1();
+                changeLevel(mPath.get(mCurrentStory));
             }
         });
 
@@ -37,9 +71,16 @@ public class MainActivity extends AppCompatActivity {
         mButtonBottom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("destini", "bottom button pressed");
+                mCurrentStory = mPath.get(mCurrentStory).getPath2();
+                changeLevel(mPath.get(mCurrentStory));
             }
         });
 
+    }
+
+    private void changeLevel(Level level) {
+        mStory.setText(mCurrentStory);
+        mButtonTop.setText(level.getAns1());
+        mButtonBottom.setText(level.getAns2());
     }
 }
